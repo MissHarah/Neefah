e2496bf6894122dc3f9cee57793cbef0a46db73a
MissHarah <motunrayor2@gmail.com>
2022-12-29 18:40:30 +0100

    first commit

---
+++ CheckingPriceApp/src/CheckingPriceApp.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+public class CheckingPriceApp {
+
+    public void display void prices() {
+
+         @Test
+                 void checkpricesOfDifferentItems() {
+             //bags
+        CheckingPriceApp checkingpriceapp = new CheckingPriceApp();
+        //when prices
+
+        }
+
+    }
+}
